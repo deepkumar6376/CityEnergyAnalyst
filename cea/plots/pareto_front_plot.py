d3675da0308438b051de4f30a44390944f66a614
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-03-21 10:17:30 +0800

    include all generations in the plot

--- cea/plots/pareto_front_plot.py
+++ cea/plots/pareto_front_plot.py
@@ -12,26 +12,27 @@ def test_graphs_optimization():
     gv = cea.globalvar.GlobalVariables()
     scenario_path = gv.scenario_reference
     locator = cea.inputlocator.InputLocator(scenario_path)
-    generation = 7
+    generation = 'all'
     os.chdir(locator.get_optimization_master_results_folder())
-
-    with open("CheckPoint" + str(generation), "rb") as csv_file:
-        pareto = []
-        xs = []
-        ys = []
-        zs = []
-        reader = csv.reader(csv_file)
-        mydict = dict(reader)
-        objective_function = mydict['objective_function_values']
-        objective_function = re.findall(r'\d+\.\d+', objective_function)
-        print (objective_function)
-        for i in xrange(gv.initialInd):
-            pareto_intermediate = [objective_function[3*i], objective_function[3*i + 1], objective_function[3*i + 2]]
-            pareto.append(pareto_intermediate)
-            xs.append(float(objective_function[3*i]))
-            ys.append(float(objective_function[3*i + 1]))
-            zs.append(float(objective_function[3*i + 2]))
-
+    pareto = []
+    xs = []
+    ys = []
+    zs = []
+    if generation is 'all':
+        for i in xrange(gv.NGEN):
+            with open("CheckPoint" + str(i + 1), "rb") as csv_file:
+                reader = csv.reader(csv_file)
+                mydict = dict(reader)
+                objective_function = mydict['objective_function_values']
+                objective_function = re.findall(r'\d+\.\d+', objective_function)
+                print (objective_function)
+                for j in xrange(gv.initialInd):
+                    pareto_intermediate = [objective_function[3 * j], objective_function[3 * j + 1],
+                                           objective_function[3 * j + 2]]
+                    pareto.append(pareto_intermediate)
+                    xs.append(float(objective_function[3 * j]))
+                    ys.append(float(objective_function[3 * j + 1]))
+                    zs.append(float(objective_function[3 * j + 2]))
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         ax.scatter(xs, ys, zs, c='r', marker='o')
@@ -60,6 +61,53 @@ def test_graphs_optimization():
         plt.savefig("Generation" + str(generation) + "Pareto_Front_2D.png")
         plt.show()
         plt.clf()
+
+    else:
+        with open("CheckPoint" + str(generation), "rb") as csv_file:
+            pareto = []
+            xs = []
+            ys = []
+            zs = []
+            reader = csv.reader(csv_file)
+            mydict = dict(reader)
+            objective_function = mydict['objective_function_values']
+            objective_function = re.findall(r'\d+\.\d+', objective_function)
+            print (objective_function)
+            for i in xrange(gv.initialInd):
+                pareto_intermediate = [objective_function[3*i], objective_function[3*i + 1], objective_function[3*i + 2]]
+                pareto.append(pareto_intermediate)
+                xs.append(float(objective_function[3*i]))
+                ys.append(float(objective_function[3*i + 1]))
+                zs.append(float(objective_function[3*i + 2]))
+
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.scatter(xs, ys, zs, c='r', marker='o')
+            ax.set_xlabel('X Label')
+            ax.set_ylabel('Y Label')
+            ax.set_zlabel('Z Label')
+            os.chdir(locator.get_optimization_plots_folder())
+            plt.savefig("Generation" + str(generation) + "Pareto_Front.png")
+            plt.show()
+
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            cm = plt.get_cmap('jet')
+            cNorm = matplotlib.colors.Normalize(vmin=min(zs), vmax=max(zs))
+            scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
+            ax.scatter(xs, ys, c=scalarMap.to_rgba(zs), s=50, alpha=0.8)
+            ax.set_xlabel('X Label')
+            ax.set_ylabel('Y Label')
+
+            scalarMap.set_array(zs)
+            fig.colorbar(scalarMap, label='Z Label')
+            plt.grid(True)
+            plt.rcParams['figure.figsize'] = (6, 4)
+            plt.rcParams.update({'font.size': 12})
+            plt.gcf().subplots_adjust(bottom=0.15)
+            plt.savefig("Generation" + str(generation) + "Pareto_Front_2D.png")
+            plt.show()
+            plt.clf()
 
 
 
