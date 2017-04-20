55b983aae8dfe7f5e0c93c8829341f112da731b0
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-04-20 11:44:31 +0800

    optimization tool interface

--- cea/interfaces/arcgis/CityEnergyAnalyst.py
+++ cea/interfaces/arcgis/CityEnergyAnalyst.py
@@ -88,6 +88,63 @@ class DemandTool(object):
 
         run_cli(scenario_path, 'demand', '--weather', weather_path)
 
+class OptimizationTool(object):
+    """integrate the optimization script with ArcGIS"""
+
+    def __init__(self):
+        self.label = 'Optimization'
+        self.description = 'Optimizing the District Energy System'
+        self.canRunInBackground = False
+
+    def getParameterInfo(self):
+        scenario_path = arcpy.Parameter(
+            displayName="Path to the scenario",
+            name="scenario_path",
+            datatype="DEFolder",
+            parameterType="Required",
+            direction="Input")
+        weather_name = arcpy.Parameter(
+            displayName="Weather file (choose from list or enter full path to .epw file)",
+            name="weather_name",
+            datatype="String",
+            parameterType="Required",
+            direction="Input")
+        weather_name.filter.list = get_weather_names()
+
+        return [scenario_path, weather_name]
+
+    def isLicensed(self):
+        return True
+
+    def updateParameters(self, parameters):
+        return
+
+    def updateMessages(self, parameters):
+        scenario_path = parameters[0].valueAsText
+        if scenario_path is None:
+            return
+        if not os.path.exists(scenario_path):
+            parameters[0].setErrorMessage('Scenario folder not found: %s' % scenario_path)
+            return
+        if not os.path.exists(get_radiation(scenario_path)):
+            parameters[0].setErrorMessage("No radiation data found for scenario. Run radiation script first.")
+        if not os.path.exists(get_surface_properties(scenario_path)):
+            parameters[0].setErrorMessage("No radiation data found for scenario. Run radiation script first.")
+        return
+
+    def execute(self, parameters, _):
+        scenario_path = parameters[0].valueAsText
+        weather_name = parameters[1].valueAsText
+        if weather_name in get_weather_names():
+            weather_path = get_weather_path(weather_name)
+        elif os.path.exists(weather_name) and weather_name.endswith('.epw'):
+            weather_path = weather_name
+        else:
+            weather_path = get_weather_path()
+
+        run_cli(scenario_path, 'demand', '--weather', weather_path)
+
+
 
 class DataHelperTool(object):
     """
