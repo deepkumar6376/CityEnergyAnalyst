8e47d10c7f5d16cf9c90d99e8915ee77ca78ded1
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-04-21 10:03:05 +0800

    hourly load profile plot

---
+++ cea/plots/hourly_load_profile_plot.py
@@ -0,0 +1,28 @@
+from __future__ import division
+
+import matplotlib
+import matplotlib.cm as cmx
+import matplotlib.pyplot as plt
+import pickle
+import deap
+
+__author__ = "Sreepathi Bhargava Krishna"
+__copyright__ = "Copyright 2017, Architecture and Building Systems - ETH Zurich"
+__credits__ = ["Sreepathi Bhargava Krishna"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Daren Thomas"
+__email__ = "cea@arch.ethz.ch"
+__status__ = "Production"
+
+def hourly_load_profile_plot(generation, individual):
+    """
+    This function plots the hourly load profile 
+    """
+
+    return
+
+if __name__ == '__main__':
+    generation = 50
+    individual = 2
+    hourly_load_profile_plot(generation, individual)
