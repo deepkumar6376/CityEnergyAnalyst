55c80378ef52f533e32b59ae0e3c2c3e52d4f86e
Daren Thomas <thomas@arch.ethz.ch>
2017-05-05 13:28:27 +0200

    first stabe at getting the interface going

---
+++ cea/interfaces/vistrails/install_vistrails_modules.py
@@ -0,0 +1,11 @@
+"""
+Install the vistrails interface by copying the files in this package to the user's vistrails module folder.
+
+This is what get's called when you do ``cea install-vistrails-modules``
+"""
+
+def install_vistrails_modules():
+    pass
+
+if __name__ == '__main__':
+    install_vistrails_modules()
