5cc396f3c02ae351dc1c27898a1de0d19a8c83f1
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-05-08 15:16:58 +0800

    minor changes

--- cea/plots/hourly_load_profile_plot.py
+++ cea/plots/hourly_load_profile_plot.py
@@ -22,7 +22,7 @@ __maintainer__ = "Daren Thomas"
 __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
-def hourly_load_profile_plot(locator, generation, individual, week):
+def hourly_load_profile_plot(locator, generation, individual, week, yearly):
     """
     This function plots the hourly load profile 
     """
@@ -61,52 +61,66 @@ def hourly_load_profile_plot(locator, generation, individual, week):
            + 'PPActivationPattern.csv'))
     df['index'] = xrange(8760)
 
+    if yearly is True:
+        index = df['index']
+
+        # Electricity Produced
+
+        ESolarProducedPVandPVT = df['ESolarProducedPVandPVT']
+        E_produced_total = df['E_produced_total']
+
+        plt.plot(index, ESolarProducedPVandPVT, 'k')
+        plt.plot(index, E_produced_total, 'c')
+        # plt.show()
+
+        # Electricity Consumed
+
+        E_GHP = df['E_GHP']
+        E_PP_and_storage = df['E_PP_and_storage']
+        E_aux_HP_uncontrollable = df['E_aux_HP_uncontrollable']
+        E_consumed_without_buildingdemand = df['E_consumed_without_buildingdemand']
+        E_building_demand = building_total['Ef_kWh']*1000
+
+        # plt.plot(index, E_GHP, 'b')
+        # plt.plot(index, E_PP_and_storage, 'r')
+        # plt.plot(index, E_aux_HP_uncontrollable, 'g')
+        # plt.plot(index, E_consumed_without_buildingdemand, 'm')
+        plt.plot(index, E_building_demand, 'm')
+        plt.show()
+
+
     df1 = df[(df['index'] >= week*7*24) & (df['index'] <= (week+1)*7*24)]
     df2 = building_total[(building_total['index'] >= week*7*24) & (building_total['index'] <= (week+1)*7*24)]
 
     fig = plt.figure()
 
-
-    ax = fig.add_subplot(111)
-    # ax.scatter(xs, ys, c=scalarMap.to_rgba(zs), s=100, alpha=0.8, vmin=0.0, vmax=1.0)
-
     index = df1['index']
+
+    # Electricity Produced
+
     ESolarProducedPVandPVT = df1['ESolarProducedPVandPVT']
+    E_produced_total = df1['E_produced_total']
+    #
+    # plt.plot(index, ESolarProducedPVandPVT, 'b')
+    plt.plot(index, E_produced_total, 'r')
+
+    # Electricity Consumed
+
     E_GHP = df1['E_GHP']
     E_PP_and_storage = df1['E_PP_and_storage']
     E_aux_HP_uncontrollable = df1['E_aux_HP_uncontrollable']
     E_consumed_without_buildingdemand = df1['E_consumed_without_buildingdemand']
-    Q_AddBoiler = df1['Q_AddBoiler']
     demand = df2['Ef_kWh']*1000
 
 
-    # plt.plot(index, ESolarProducedPVandPVT, 'g')
-    plt.plot(index, E_GHP, 'b')
-    plt.plot(index, E_PP_and_storage, 'r')
-    # plt.plot(index, E_aux_HP_uncontrollable, 'o')
-    # plt.plot(index, E_consumed_without_buildingdemand, 'c')
-    plt.plot(index, demand, 'c')
-    plt.show()
-    # pdf.savefig()
-
-    # index = df['index']
-    # ESolarProducedPVandPVT = df['ESolarProducedPVandPVT']
-    # E_GHP = df['E_GHP']
-    # E_PP_and_storage = df['E_PP_and_storage']
-    # E_aux_HP_uncontrollable = df['E_aux_HP_uncontrollable']
-    # E_consumed_without_buildingdemand = df['E_consumed_without_buildingdemand']
-    # Q_AddBoiler = df['Q_AddBoiler']
-    # demand = building_total['Ef_kWh']
-    #
-    #
     # plt.plot(index, ESolarProducedPVandPVT, 'g')
     # plt.plot(index, E_GHP, 'b')
     # plt.plot(index, E_PP_and_storage, 'r')
     # plt.plot(index, E_aux_HP_uncontrollable, 'o')
     # plt.plot(index, E_consumed_without_buildingdemand, 'c')
-    # # plt.plot(index, Q_AddBoiler, 'c')
-    # plt.plot(index, demand, 'r')
-    # plt.show()
+    plt.plot(index, demand, 'c')
+    plt.show()
+    # pdf.savefig()
 
     return
 
@@ -117,8 +131,9 @@ if __name__ == '__main__':
     locator = cea.inputlocator.InputLocator(scenario_path)
 
     generation = 5
-    individual = 2
+    individual = 1
+    yearly = True
     week = 5
 
     individual = individual - 1
-    hourly_load_profile_plot(locator, generation, individual, week)
+    hourly_load_profile_plot(locator, generation, individual, week, yearly)
