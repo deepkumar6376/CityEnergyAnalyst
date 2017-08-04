786f0cef565cc36efb1ee1a9e48067b7a5abecd7
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-08-04 19:04:52 +0800

    testing

--- cea/optimization/optimization_settings.py
+++ cea/optimization/optimization_settings.py
@@ -14,8 +14,8 @@ __status__ = "Production"
 class optimization_settings(object):
     def __init__(self):
 
-        self.initialInd = 2  # number of initial individuals
-        self.NGEN = 5  # number of total generations
+        self.initialInd = 10  # number of initial individuals
+        self.NGEN = 50  # number of total generations
         self.fCheckPoint = 1  # frequency for the saving of checkpoints
         self.maxTime = 7 * 24 * 3600  # maximum computational time [seconds]
 
